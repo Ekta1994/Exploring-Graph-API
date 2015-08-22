1841e51c08b042480a191a5f53470e18211664f3
unknown <ektagoel04@gmail.com>
2015-08-23 00:48:12 +0530

    Modified comments

--- fbfriends.py
+++ fbfriends.py
@@ -20,13 +20,13 @@ TOKEN = "Your generated token here";
 def getpics():
 
 	"""
-		Generate the FQL query. The result is a combination of dictionaries and lists inside one another. For manually testing it, go on the link provided in readme.md and click on FQL query and type the query below in the space provided.
+	Generate the FQL query. The result is a combination of dictionaries and lists inside one another. For manually testing it, go on the link provided in readme.md and click on FQL query and type the query below in the space provided.
 	"""
     query= ("SELECT first_name, last_name , pic FROM user WHERE uid IN  (SELECT uid2 FROM friend WHERE uid1 = me() ) LIMIT 5")
     payload = {'q' : query , 'access_token' : TOKEN}
 	
 	"""
-		The requests.get() method needs the URL as a parameter. For appending key value pairs to this URL, the keyword params is used.
+	The requests.get() method needs the URL as a parameter. For appending key value pairs to this URL, the keyword params is used.
 	"""
 	
     r = requests.get("https://graph.facebook.com/fql", params = payload)
@@ -34,7 +34,7 @@ def getpics():
     
     cnt = 0
 	"""
-		This command creates a folder named ProfilePics in your current working directory and all the profile pictures downloaded will be saved in this folder.
+	This command creates a folder named ProfilePics in your current working directory and all the profile pictures downloaded will be saved in this folder.
 	"""
     imagepath = os.getcwd() + '\ProfilePics' ;
     os.chdir(imagepath)
